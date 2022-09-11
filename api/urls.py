19fa275e28010351f2769d7bc3fcd11c48637a9c
stjonas <jonasstoegerer@yahoo.at>
2022-09-11 20:59:39 +0700

    setup, first routes and models

---
+++ api/urls.py
@@ -0,0 +1,8 @@
+from . import views
+from django.urls import path
+
+urlpatterns = [
+    path('', views.getRoutes, name='routes'),
+    path('notes/', views.getNotes, name='notes'),
+    path('notes/<str:pk>/', views.getNote, name='notes')
+]
